3be0ddeb2747cb76d9e7a9117a90591ac9385b3a
paj33tooor <139661626+pajeetoor@users.noreply.github.com>
2025-01-06 11:03:05 -0500

    Update and rename binance.java to Binance.java

--- Binance.java
+++ Binance.java
@@ -1,4 +1,4 @@
-public class AsciiArt {
+public class Binance {
     public static void main(String[] args) {
         String asciiArt = 
                 "                                                                                          \n" +
