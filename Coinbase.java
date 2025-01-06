6c7d3e4fab7c796bec134117ec66faeeb7dc1b37
paj33tooor <139661626+pajeetoor@users.noreply.github.com>
2025-01-06 11:04:51 -0500

    Update and rename coinbase to Coinbase.java

--- Coinbase.java
+++ Coinbase.java
@@ -1,4 +1,4 @@
-public class AsciiArt {
+public class Coinbase {
     public static void main(String[] args) {
         String asciiArt = 
                 "                                   .::--====+++====--:.                                    \n" +
