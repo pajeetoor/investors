ef4c13d1e2860422741310f9acdf8fe8311efa34
paj33tooor <139661626+pajeetoor@users.noreply.github.com>
2025-01-06 11:05:49 -0500

    Update and rename paradigm to Paradigm.java

--- Paradigm.java
+++ Paradigm.java
@@ -1,4 +1,4 @@
-public class AsciiArt {
+public class Paradigm {
     public static void main(String[] args) {
         String asciiArt = 
                 "                                                                                          \n" +
